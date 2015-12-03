203fef2459a66c3b4216433a6d2e122d9812d559
GuyShane <brasss@hotmail.com>
2015-12-02 19:48:22 -0700

    Playing around with a possible background

--- core/src/com/shane/tapit/GameScreen.java
+++ core/src/com/shane/tapit/GameScreen.java
@@ -3,9 +3,13 @@ package com.shane.tapit;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -20,9 +24,16 @@ public class GameScreen extends ScreenAdapter {
     private ShapeRenderer sr;
     private BubbleMaster bubbles;
     private InputHandler in;
+    private AssetManager am;
+    private Texture bgTexture;
+    private SpriteBatch batch;
+    private float y=0;
 
     public GameScreen(Game game) {
+        am=new AssetManager();
+        am.load("bg.png",Texture.class);
         this.game=game;
+        am.finishLoading();
     }
 
     @Override
@@ -34,12 +45,20 @@ public class GameScreen extends ScreenAdapter {
         sr=new ShapeRenderer();
         bubbles=new BubbleMaster();
         in=new InputHandler(viewport);
+        batch=new SpriteBatch();
+        bgTexture=am.get("bg.png");
     }
 
     @Override
     public void render(float delta) {
+        y-=10*delta;
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(camera.projection);
+        batch.setTransformMatrix(camera.view);
+        batch.begin();
+        batch.draw(bgTexture,0,0,1280,6400,0,(int)y,640,3200,false,false);
+        batch.end();
         if (in.isTouching() && in.isNewTouchEvent()) {
             if (bubbles.shouldPop(in.getTouch())) {
                 bubbles.pop();
